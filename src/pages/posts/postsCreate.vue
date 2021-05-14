71a4e3b115c20d7ff73803cc0b44e9bb06a8764d
juliadelisposte <loretta_deli@hotmail.com>
2021-05-14 14:14:02 -0300

    fixes

--- src/pages/posts/postsCreate.vue
+++ src/pages/posts/postsCreate.vue
@@ -9,7 +9,7 @@
         </div>
       </div>
       <div class="row justify-center">
-        <div class="col-1">
+        <div class="col-1 q-mt-md">
           <q-btn-dropdown size="13px" color="primary" icon="add_a_photo" stack glossy label="Imagem">
             <q-item clickable>
               <q-item-section>
@@ -21,18 +21,18 @@
             </q-item>
           </q-btn-dropdown>
         </div>
-        <div class="col-2 q-mx-sm q-mt-sm">
+        <div class="col-2 q-mx-sm q-my-lg">
           <q-select filled v-model="values.modelAuthor" :options="optionsAuthor" label="Autor" />
         </div>
-        <div class="col-2 q-mx-sm q-mt-sm">
+        <div class="col-2 q-mx-sm q-my-lg">
           <q-select filled v-model="values.modelCategory" :options="optionsCategory" label="Categoria" />
         </div>
-        <div class="col-2 q-mx-sm q-mt-sm">
-          <q-input filled label="Insira a data" v-model="values.date" mask="date">
+        <div class="col-2 q-mx-sm q-my-lg">
+          <q-input filled label="Insira a data" v-model="values.date">
             <template v-slot:append>
               <q-icon name="event" class="cursor-pointer">
                 <q-popup-proxy transition-show="scale" transition-hide="scale">
-                  <q-date v-model="values.date">
+                  <q-date v-model="values.date" mask="DD/MM/YYYY">
                     <div class="row items-center justify-end">
                       <q-btn v-close-popup label="Fechar" color="primary" />
                     </div>
@@ -44,12 +44,12 @@
         </div>
           <div class="col col-md-8">
             <div class="pages-posts-create__post-area">
-              <q-input class="q-pr-lg q-pt-md" v-model="values.inputDescription" color="secondary" clearable filled label="Insira a pequena descrição do seu post" />
-              <q-input class="q-pr-lg q-pt-md q-mb-md" v-model="values.inputPost" color="secondary" filled type="textarea" label="Insira o conteúdo do seu post" />
+              <q-input class="q-pr-lg" v-model="values.inputDescription" color="secondary" clearable filled label="Insira a pequena descrição do seu post" />
+              <q-input class="q-pr-lg q-mb-md q-mt-lg" v-model="values.inputPost" color="secondary" filled type="textarea" label="Insira o conteúdo do seu post" />
             </div>
           </div>
       </div>
-      <div class="row justify-center">
+      <div class="row justify-center q-mt-lg">
         <q-btn color="primary" icon-right="send" label="Postar" />
       </div>
     </div>
@@ -57,6 +57,7 @@
 </template>
 
 <script>
+
 export default {
   data () {
     return {
@@ -66,9 +67,7 @@ export default {
         inputPost: '',
         url: '',
         modelCategory: 'Escolha a categoria',
-        modelAuthor: 'Escolha o autor',
-        secondDate: '2021/04/30',
-        date: '2021/04/29'
+        modelAuthor: 'Escolha o autor'
       },
       optionsCategory: [
         'Categoria 1', 'Categoria 2', 'Categoria 3', 'Categoria 4', 'Categoria 5'
