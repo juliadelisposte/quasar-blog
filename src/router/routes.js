b526482a76ff47eb61f90d26de2bacc0520778ae
juliadelisposte <loretta_deli@hotmail.com>
2021-06-10 18:20:39 -0300

    fixes

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,12 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/posts/postsList.vue'), name: 'PostsList' },
-      { path: 'posts/create-posts', component: () => import('pages/posts/PostsForm.vue'), name: 'PostsCreate' },
-      { path: 'posts/:id/posts-edit', component: () => import('src/pages/posts/PostsForm.vue'), name: 'PostsEdit' },
-      { path: 'authors-list', component: () => import('pages/authors/authorsList'), name: 'AuthorsList' },
-      { path: 'authors-form', component: () => import('pages/authors/authorsForm'), name: 'AuthorsForm' },
-      { path: 'posts/:id', component: () => import('pages/posts/postsSingle.vue'), name: 'PostsSingle' }
+      { path: 'authors/new', component: () => import('pages/authors/AuthorsForm'), name: 'AuthorsCreate' },
+      { path: 'authors', component: () => import('pages/authors/AuthorsList'), name: 'AuthorsList' }
     ]
   },
   {
