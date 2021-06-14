db9b95a5c6168c78fdb7dcdd776ed85b8bd3ba34
juliadelisposte <loretta_deli@hotmail.com>
2021-06-14 16:12:03 -0300

    fixes

--- src/pages/authors/AuthorsForm.vue
+++ src/pages/authors/AuthorsForm.vue
@@ -3,7 +3,7 @@
     <h1 class="text-h5 row justify-center q-py-md text-primary">Criação de Autores</h1>
     <div class="row justify-center">
       <div class="col-md-8 q-pt-sm">
-        <q-form class="q-gutter-md" @submit="saveAuthor(createAuthors)">
+        <q-form class="q-gutter-md" @submit="submit()">
           <q-input filled v-model="values.name" label="Insira o nome do autor" hint="Nome e Sobrenome"/>
           <q-input filled v-model="values.email" label="Insira o e-mail do autor" type="email"/>
           <div class="row justify-center q-pt-sm">
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-
+import { mapActions } from 'vuex'
 export default {
   data () {
     return {
@@ -28,7 +28,11 @@ export default {
   },
 
   methods: {
-    saveAuthor () {
+    ...mapActions({
+      createAuthors: 'authors/createAuthors'
+    }),
+
+    submit () {
       this.createAuthors(this.values)
     }
   }
