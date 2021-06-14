db06a04d21cb7fd0790a09c5f5f2e5a5490d23d8
juliadelisposte <loretta_deli@hotmail.com>
2021-06-14 16:17:50 -0300

    fixes

--- src/pages/authors/AuthorsForm.vue
+++ src/pages/authors/AuthorsForm.vue
@@ -3,7 +3,7 @@
     <h1 class="text-h5 row justify-center q-py-md text-primary">Criação de Autores</h1>
     <div class="row justify-center">
       <div class="col-md-8 q-pt-sm">
-        <q-form class="q-gutter-md" @submit="submit()">
+        <q-form class="q-gutter-md" @submit="submit">
           <q-input filled v-model="values.name" label="Insira o nome do autor" hint="Nome e Sobrenome"/>
           <q-input filled v-model="values.email" label="Insira o e-mail do autor" type="email"/>
           <div class="row justify-center q-pt-sm">
